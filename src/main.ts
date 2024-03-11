2c3a0ae8325e7ef44f38bb9345d5c87f3527d19d
Nicolas Walter <nicolaswalter2003@gmail.com>
2024-03-11 15:25:27 +0100

    new assets

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,27 @@
-import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
+import { Boot } from "./scenes/Boot";
+import { Game as MainGame } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import { Preloader } from "./scenes/Preloader";
 
 import { Game, Types } from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+  type: Phaser.AUTO,
+  width: 972,
+  height: 540,
+  backgroundColor: "#fff",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300, x: 0 },
+      debug: false,
     },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  },
+  parent: "game-container",
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 export default new Game(config);
--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,22 +1,24 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Boot extends Scene
-{
-    constructor ()
-    {
-        super('Boot');
-    }
+export class Boot extends Scene {
+  constructor() {
+    super("Boot");
+  }
 
-    preload ()
-    {
-        //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
-        //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
+  preload() {
+    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
+    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
-        this.load.tilemapTiledJSON('map', 'assets/Tiled/map.json');
-    }
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("island", "assets/island.png");
+    this.load.spritesheet("player", "assets/player.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+    // this.load.tilemapTiledJSON('map', 'assets/Tiled/map.json');
+  }
 
-    create ()
-    {
-        this.scene.start('Preloader');
-    }
+  create() {
+    this.scene.start("Preloader");
+  }
 }
